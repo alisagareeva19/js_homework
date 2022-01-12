e7e3c21eb3fcf18978b1ba19277936e5c37a5f35
alisagareeva19 <alisagareeva19@gmail.com>
2022-01-13 00:56:21 +0500

    HM 1 and 2

---
+++ week13/index13_1.js
@@ -0,0 +1,4 @@
+function Name() {
+    let name = prompt('Введите ваше имя:', '...');
+    alert(`Привет, ${name}!`);
+};
---
+++ week13/index13_2.js
@@ -0,0 +1,27 @@
+// function Enter() {
+//     let x = prompt('Введите первое число:', '');
+//     let y = prompt('Введите второе число:', '');
+// }
+
+function calcSum() {
+    let x = prompt('Введите первое слагаемое:', '');
+    let y = prompt('Введите второе слагаемое:', '');
+    alert("Результат сложения: " + (Number(x) + Number(y)));
+}
+
+function calcSub() {
+    let x = prompt('Введите уменьшаемое:', '');
+    let y = prompt('Введите вычитаемое:', '');
+    alert("Результат вычитания: " + (Number(x) - Number(y)));
+}
+function calcMult() {
+    let x = prompt('Введите первый множитель:', '');
+    let y = prompt('Введите второй множитель:', '');
+    alert("Результат умножения: " + (Number(x) * Number(y)));
+}
+
+function calcDiv() {
+    let x = prompt('Введите делимое:', '');
+    let y = prompt('Введите делитель:', '');
+    alert("Результат деления: " + (Number(x) / Number(y)));
+}
