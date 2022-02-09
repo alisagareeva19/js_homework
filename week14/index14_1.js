7aa6a610220e1b7cb678dd668435de1bbc158194
alisagareeva19 <alisagareeva19@gmail.com>
2022-02-09 22:17:49 +0500

    debugger

--- week14/index14_1.js
+++ week14/index14_1.js
@@ -18,6 +18,7 @@ function calcMult() {
     let num2 = document.getElementById('num2').value;
     let otvet;
     otvet = Number(num1) * Number(num2);
+    // console.log(otvet)
     document.getElementById('otvet').value = otvet;
 }
 function calcDiv() {
